1fc282bd2ce8648d7794074463a0452bc58763da
IshaniSirinayake <ishani.sirinayake@synapsys.lk>
2024-02-25 00:07:20 +0530

    ui fixes

--- src/components/Pages/login.tsx
+++ src/components/Pages/login.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import * as Yup from "yup";
 import { Form, Formik } from "formik";
 import { ILogin } from "../../typings/interfaces/ILogin";
@@ -16,7 +16,12 @@ import { VENDOR_SEARCH_ROUTE } from "../../constants/routes";
 import { authenticate } from "../../store/auth/authAction";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
 import './style.css';
-
+import { makeStyles } from '@material-ui/core/styles';
+import logo from "../../assets/logo-white-bg.png";
+const useStyles = makeStyles({
+  
+  
+});
 const loginSchema = Yup.object().shape({
   email: Yup.string()
     .required(REQUIRED_MSG)
@@ -30,6 +35,7 @@ const loginSchema = Yup.object().shape({
 });
 
 const Login = () => {
+  const classes = useStyles();
   const [value, setValue] = useState<ILogin>({ email: "", password: "" });
   const user = useAppSelector((state) => state.auth)
   const dispatch = useAppDispatch();
@@ -44,69 +50,87 @@ const Login = () => {
   // },[user])
 
   return (
-    <Box className="boxstly" sx={{ display: "flex", marginLeft: "10%", alignContent: "center", height: "calc(80vh - 20px)", width: "80%"}}>
-      <Box sx={{ width: "400px", margin: "auto", borderRadius: "20px" }}>
-          {!user.authResponse ?
-          <FormContainer formTitle="Please Login Here">
-            <Formik
-              initialValues={value}
-              validationSchema={loginSchema}
-              onSubmit={handleLogin}
-            >
-              {(props) => (
-                <Form>
-                  <FieldContainer label="Email" sx={{marginBottom:3}}>
-                    <TextField
-                      name="email"
-                      size="small"
-                      placeholder={"Email"}
-                      fullWidth
-                      value={props.values.email}
-                      error={props.errors.email === undefined ? false : true}
-                      helperText={props.errors.email}
-                      onChange={(e) => {
-                        props.setFieldValue(e.target.name, e.target.value);
-                      }}
-                    />
-                  </FieldContainer>
-                  <FieldContainer label="Password">
-                    <TextField
-                      name="password"
-                      type="password"
-                      size="small"
-                      placeholder={"Password"}
-                      fullWidth
-                      value={props.values.password}
-                      error={props.errors.password === undefined ? false : true}
-                      helperText={props.errors.password}
-                      onChange={(e) => {
-                        props.setFieldValue(e.target.name, e.target.value);
-                      }}
-                    />
-                  </FieldContainer>
+    
+      <Box className="boxstly" sx={{ display: "flex", marginLeft: "10%", alignContent: "center",  width: "80%", height:"auto"}}>
+      
+        <Box className="form" sx={{ width: "500px", margin: "auto"}}>
+            <div><img className="logo" src={logo} width={50}/></div>
+            <Typography variant="h5" sx={{color:"#8C3C99", fontWeight:"700"}}>
+                Queue Management System
+            </Typography>
+            <Typography variant="h6" sx={{color:"#8C3C99", fontWeight:"700", marginTop: "4%", marginLeft: "40%", marginBottom: "4%", alignContent: "center"}}>
+                Login
+            </Typography>
+            <Typography sx={{color:"#8C3C99", fontWeight:"700"}}>Welcome Back ! Please login to account.</Typography>
+            {!user.authResponse ?           
+            <FormContainer className="form" formTitle="" sx={{border: "none"}}>
+               
+              <Formik
+                initialValues={value}
+                validationSchema={loginSchema}
+                onSubmit={handleLogin}
+              >
+                {(props) => (
+                  <Form>
+                    <FieldContainer label="Email" sx={{marginBottom:3}}>
+                      <TextField
+                        name="email"
+                        size="small"
+                        placeholder={"Email"}
+                        fullWidth
+                        value={props.values.email}
+                        error={props.errors.email === undefined ? false : true}
+                        helperText={props.errors.email}
+                        onChange={(e) => {
+                          props.setFieldValue(e.target.name, e.target.value);
+                        }}
+                      />
+                    </FieldContainer>
+                    <FieldContainer label="Password">
+                      <TextField
+                        name="password"
+                        type="password"
+                        size="small"
+                        placeholder={"Password"}
+                        fullWidth
+                        value={props.values.password}
+                        error={props.errors.password === undefined ? false : true}
+                        helperText={props.errors.password}
+                        onChange={(e) => {
+                          props.setFieldValue(e.target.name, e.target.value);
+                        }}
+                      />
+                    </FieldContainer>
 
-                  <Button variant="contained" color="success" type="submit" disabled={!props.isValid || user.authResponseLoading}>
-                    Login
-                  </Button>
-                </Form>
-              )}
-            </Formik>
-            <FieldContainer label="">
-            {user.authResponseError && <AlertBox message={user.authResponseError.response.data.error} severity="error" showAlert /> } 
-            </FieldContainer>
+                    <Button sx={{background:"#8C3C99", width:"200px", marginRight:"20px"}} variant="contained"  type="submit" disabled={!props.isValid || user.authResponseLoading}>
+                      Login
+                    </Button>
+                    <Button sx={{background:"#fff", color:"#8C3C99", '&:hover': {                        
+                        color: '#fff',
+                      }, width:"200px", border:"1px solid #8C3C99"}} 
+                      variant="contained"  type="submit" disabled={!props.isValid || user.authResponseLoading}>
+                      Sign Up
+                    </Button>
+                  </Form>
+                )}
+              </Formik>
+              <FieldContainer label="">
+              {user.authResponseError && <AlertBox message={user.authResponseError.response.data.error} severity="error" showAlert /> } 
+              </FieldContainer>
+            
+            </FormContainer>
+            : 
+            <FieldContainer label="" sx={sxMainMessage}>You are already logged! Go to <Link to={VENDOR_SEARCH_ROUTE}>Home</Link></FieldContainer>
+          }
           
-          </FormContainer>
-          : 
-          <FieldContainer label="" sx={sxMainMessage}>You are already logged! Go to <Link to={VENDOR_SEARCH_ROUTE}>Home</Link></FieldContainer>
-        }
-        
+        </Box>
+        <Box className="purple" sx={{ backgroundColor: "#CF9DD7", width: "650px", overflow:"hidden" }}>
+          <div className="image">
+            <img className="banner" src={require('../../assets/queue1.png')} />
+          </div>
+        </Box>
       </Box>
-      <Box className="purple" sx={{ backgroundColor: "#CF9DD7", width: "650px", overflow:"hidden" }}>
-        <div className="image">
-          <img src={require('../../assets/queue1.png')} />
-        </div>
-      </Box>
-    </Box>
+    
   );
 };
 
